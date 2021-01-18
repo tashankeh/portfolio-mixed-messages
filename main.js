38a3749fe1b91c61388816b9591cb20311c90949
Tasha A. NKEH <tashankeh@gmail.com>
2021-01-18 23:46:52 +0100

    further streamlining of code

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 // code for the mixed messages portfolio project
 
 // Message object
-const message = {
-    _msgYear: [],
-    wish: ["will certainly be your"," if well planned, will be your", "if nothing is done now will not be your",
-          "will be your", "seems to be your","will be your siter's","will be your son's","will be your daughter's","will be your Mama's","will be Dad's"],
-    qualification:  ["health & wellness","financial freedom","networking & relationships",
+const msg = {
+    _year: [],
+    ref: ["will certainly be your"," if well planned, will be your", "if nothing is done now will not be your",
+          "will be your", "seems to be your","will be your siter's","will be your son's",
+          "will be your daughter's","will be your Mama's","will be Dad's"],
+    wish:  ["health & wellness","financial freedom","networking & relationships building",
           "spiritual awareness & growth","relaxation & enjoyment","massive financial investment",
           "harvesting the fruits of consistency","joy & laughter",
           "spiritual regeneration"," sacrifice & service to humanity"],
-    set msgYear(startYear){
+    set year(firstYear){
         let arrYears = [];
         for (i= 0; i<10; i++) {
-            arrYears.push(startYear);
-            startYear ++;
+            arrYears.push(firstYear);
+            firstYear ++;
         }
-        this._msgYear = arrYears;
+        this._year = arrYears;
     },
-    get msgYear() {
-        return this._msgYear;
+    get year() {
+        return this._year;
     }
 };
 
 //generate a random index
-const randomNumGen = num =>{
+const indexGen = num =>{
     let index = Math.floor(Math.random()*num);
     return index;
 }
 //msg generation function
 
-const mixedMessages = (yr) => {
-    message.msgYear = yr;
-    let x = randomNumGen(message.msgYear.length);
-    let y = randomNumGen(message.wish.length);
-    let z = randomNumGen(message.qualification.length);
-    console.log(`${message.msgYear[x]} ${message.wish[y]} year of ${message.qualification[z]}`);
+const printMixedMsg = (startYr) => {
+    msg.year = startYr;
+    let x = indexGen(msg.year.length);
+    let y = indexGen(msg.ref.length);
+    let z = indexGen(msg.wish.length);
+    console.log(`${msg.year[x]} ${msg.ref[y]} year of ${msg.wish[z]}`);
 }
-mixedMessages(2021);
+printMixedMsg(2021);
